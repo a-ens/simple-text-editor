af138dc6f8a178bf03401b2173c8184573a6feea
aenz <andrewjensor@gmail.com>
2022-12-05 15:43:32 -0500

    customizing pwa manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,25 +19,21 @@ module.exports = () => {
     },
     plugins: [
       new WebpackPwaManifest({
-        name: 'My Progressive Web App',
+        name: 'simple-text-edtor',
         short_name: 'MyPWA',
-        description: 'My awesome Progressive Web App!',
-        background_color: '#ffffff',
-        crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+        description: 'pwa text editor',
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        fingerprints: false,
+        inject: true,
         icons: [
           {
-            src: path.resolve('src/assets/icon.png'),
-            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+            destination: path.join('assets', 'icons')
           },
-          {
-            src: path.resolve('src/assets/large-icon.png'),
-            size: '1024x1024' // you can also use the specifications pattern
-          },
-          {
-            src: path.resolve('src/assets/maskable-icon.png'),
-            size: '1024x1024',
-            purpose: 'maskable'
-          }
         ],
       })
     ],
